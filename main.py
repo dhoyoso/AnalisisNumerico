fbf79bf576e9d8b2100e5485d6b1cfd091028122
Daniel Hoyos Ospina <dhoyoso@eafit.edu.co>
2018-05-11 11:08:20 -0500

    interfaz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-import matplotlib.pyplot as plt
+
 import numpy as np
 from Funciones import Funciones
 from PyQt5.QtCore import pyqtSlot
@@ -13,28 +13,7 @@ from paramPunto import paramPunto
 from paramRaices import paramRaices
 from paramRegla import paramRegla
 from paramSecante import paramSecante
-
 from Datos import datos
-
-## Create functions and set domain length
-x = np.arange(0.0, 2.0, 0.01)
-y = x**2
-dy = 2*x - 1
-
-## Plot functions and a point where they intersect
-plt.plot(x, y)
-plt.plot(x, dy)
-plt.plot(1, 1, 'or')
-
-## Config the graph
-plt.title('A Cool Graph')
-plt.xlabel('X')
-plt.ylabel('Y')
-#plt.ylim([0, 4])
-plt.grid(True)
-plt.legend(['y = x^2', 'y = 2x'], loc='upper left')
-
-## Show the graph
 
 
 class main(QDialog):
