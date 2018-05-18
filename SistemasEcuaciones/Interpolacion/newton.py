e5eccaf461a902695a4d29fbfe07bd2e05806168
Daniel Hoyos Ospina <dhoyoso@eafit.edu.co>
2018-05-18 15:55:11 -0500

    newton changes

--- SistemasEcuaciones/Interpolacion/newton.py
+++ SistemasEcuaciones/Interpolacion/newton.py
@@ -6,10 +6,9 @@ class NewtonInterpolacion:
         self.valor = 0
         self.n = 0
 
-    def newtonInterpolacion(self, nroPtos, valor, x, y):
+    def newtonInterpolacion(self, nroPtos, x, y):
         self.x = x
         self.y = y
-        self.valor = valor
         self.n = nroPtos
         self.tabla = [[0] * (self.n) for i in range(self.n)]
         for i in range(0, self.n):
@@ -33,13 +32,18 @@ class NewtonInterpolacion:
             aux = aux * (self.valor - x[i - 1])
             bi = bi + self.tabla[i][i] * aux
         print(pol)
-        print("Resultado: ")
-        print("f(" + str(valor) + ") = " + str(bi))
 
     def imprimirMatriz(self):
-
-        # print('     '.join(str(self.marcas)))
         print('\n'.join(['     '.join(['{:4}'.format(round(item, 2)) for item in row]) for row in self.tabla]))
+
+    def hallarvalor(self):
+        bi = self.tabla[0][0]
+        aux = 1
+        for i in range(1, self.n):
+            aux = aux * (self.valor - self.x[i - 1])
+            bi = bi + self.tabla[i][i] * aux
+        return bi
+
 
     def reset(self):
         self.xns = []
@@ -49,7 +53,7 @@ class NewtonInterpolacion:
 
 gausi = NewtonInterpolacion()
 
-# x = [1, 1.2, 1.4, 1.6, 1.8]
-# y = [-0.620907, 0.640927, 2.234099, 4.183599, 6.513606]
-#
-# gausi.newtonInterpolacion(5, 2, x, y)
+x = [1, 1.2, 1.4, 1.6, 1.8]
+y = [-0.620907, 0.640927, 2.234099, 4.183599, 6.513606]
+
+gausi.newtonInterpolacion(5, x, y)
